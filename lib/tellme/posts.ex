1a5dd082eaa4bf4ca48662467bc166adb85c5041
Allen Reed <86163102+Allen-Reed@users.noreply.github.com>
2024-11-04 13:51:35 -0600

    Finish functionality and use PubSub

--- lib/tellme/posts.ex
+++ lib/tellme/posts.ex
@@ -1,11 +1,22 @@
 defmodule Tellme.Posts do
+  import Ecto.Query, warn: false
+
   alias Tellme.Repo
   alias Tellme.Posts.Post
 
+  def list_posts do
+    query =
+      from p in Post,
+        select: p,
+        order_by: [desc: :inserted_at],
+        preload: [:user]
+
+    Repo.all(query)
+  end
+
   def save(post_params) do
-    %Posts{}
+    %Post{}
     |> Post.changeset(post_params)
     |> Repo.insert()
   end
-
 end
--- lib/tellme_web/live/home_live.ex
+++ lib/tellme_web/live/home_live.ex
@@ -5,10 +5,28 @@ defmodule TellmeWeb.HomeLive do
   alias Tellme.Posts.Post
 
   @impl true
+  def render(%{loading: true} = assigns) do
+    ~H"""
+    Loading up...
+    """
+  end
+
   def render(assigns) do
     ~H"""
     <h1 class="text-2xl">Feed</h1>
     <.button type="button" phx-click={show_modal("new-post-modal")}>New Post</.button>
+
+    <div id="feed" phx-update="stream" class="flex flex-col gap-2">
+      <div
+        :for={{dom_id, post} <- @streams.posts}
+        id={dom_id}
+        class="w-1/2 mx-auto flex flex-col gap-2 p-4 border rounded"
+      >
+        <img src={post.image_path} />
+        <p><%= post.user.email %></p>
+        <p><%= post.caption %></p>
+      </div>
+    </div>
 
     <.modal id="new-post-modal">
       <.simple_form for={@form} phx-change="validate" phx-submit="save-post">
@@ -23,17 +41,24 @@ defmodule TellmeWeb.HomeLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    form =
-      %Post{}
-      |> Post.changeset(%{})
-      |> to_form(as: "post")
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(Tellme.PubSub, "posts")
 
-    socket =
-      socket
-      |> assign(form: form)
-      |> allow_upload(:image, accept: ~w(.png .jpg), max_entries: 1)
+      form =
+        %Post{}
+        |> Post.changeset(%{})
+        |> to_form(as: "post")
 
-    {:ok, socket}
+      socket =
+        socket
+        |> assign(form: form, loading: false)
+        |> allow_upload(:image, accept: ~w(.png .jpg), max_entries: 1)
+        |> stream(:posts, Posts.list_posts())
+
+      {:ok, socket}
+    else
+      {:ok, assign(socket, loading: true)}
+    end
   end
 
   @impl true
@@ -47,33 +72,47 @@ defmodule TellmeWeb.HomeLive do
 
     post_params
     |> Map.put("user_id", user.id)
-    |> Map.put("image_path", List.first(handle_event(socket)))
+    |> Map.put("image_path", List.first(consume_files(socket)))
     |> Posts.save()
     |> case do
-      {:ok, _post} ->
+      {:ok, post} ->
+        # Debugging line
+        IO.puts("Post saved successfully!")
+
         socket =
           socket
           |> put_flash(:info, "Post created successfully!")
+          |> stream_insert(:posts, post)
           |> push_navigate(to: ~p"/home")
+
+        Phoenix.PubSub.broadcast(Tellme.PubSub, "posts", {:new, Map.put(post, :user, user)})
+
         {:noreply, socket}
 
-
-      {:error, _changeset}
+      {:error, changeset} ->
+        IO.puts("Post failed to save")
+        IO.inspect(changeset)
         {:noreply, socket}
     end
+  end
+
+  @impl true
+  def handle_info({:new, post}, socket) do
+    socket =
+      socket
+      |> put_flash(:info, "#{post.user.email} just posted!")
+      |> stream_insert(:posts, post, at: 0)
 
     {:noreply, socket}
   end
 
-  @impl Phoenix.LiveView
-  def handle_event("save", _params, socket) do
-    uploaded_files =
-      consume_uploaded_entries(socket, :avatar, fn %{path: path}, _entry ->
-        dest = Path.join(Application.app_dir(:tellme, "priv/static/uploads"), Path.basename(path))
-        File.cp!(path, dest)
-        {:ok, ~p"/uploads/#{Path.basename(dest)}"}
-      end)
+  defp consume_files(socket) do
+    consume_uploaded_entries(socket, :image, fn %{path: path}, _entry ->
+      dest = Path.join([:code.priv_dir(:tellme), "static", "uploads", Path.basename(path)])
 
-    {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+      File.cp!(path, dest)
+
+      {:postpone, ~p"/uploads/#{Path.basename(dest)}"}
+    end)
   end
 end
