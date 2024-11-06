49574f51a341afd71a7185c4d45ba179b55336e8
Allen Reed <86163102+Allen-Reed@users.noreply.github.com>
2024-11-06 08:43:14 -0600

    Fix white on white text

--- lib/tellme_web/components/core_components.ex
+++ lib/tellme_web/components/core_components.ex
@@ -377,7 +377,7 @@ defmodule TellmeWeb.CoreComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full rounded-lg text-white focus:ring-0 sm:text-sm sm:leading-6",
+          "mt-2 block w-full rounded-lg text-white focus:ring-0 sm:text-sm sm:leading-6 bg-gray-700",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
